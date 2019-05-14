8d08d4d6d9ff526acc207c59de7b6be1ec16f548
wangxulong <duanxian0402@126.com>
2019-05-14 19:52:58 +0800

    init

--- builder.sh
+++ builder.sh
@@ -1,15 +1,43 @@
 #!/usr/bin/env bash
 
-
 REGISTRY=image.overlay2.lsn.im
-
 action=$1
+branch=$(git rev-parse --abbrev-ref HEAD)
+deploy_dir="dev"
+
+echo ${branch}
+
+if [[ ${branch}  =~ ^v[1-9]+\.[0-9]+\.[0-9]+$ ]]; then
+     echo "发布版"
+
+    #发布版
+    version=$(git rev-parse --abbrev-ref HEAD)
+    deploy_dir="release"
+else
+    echo "最先版本"
+    #version=${branch}-$(git rev-parse --short HEAD)
+    version="latest"
+fi
+
+#function action_build() {
+#    docker run --rm -w /app -v $HOME/.m2:/root/.m2 -v `pwd`:/app \
+#        maven:3.6.1-jdk-11-slim \
+#        mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${version}
+#    #编译，运行单元测试和集成测试
+#    cd dev && ./run.sh
+#}
+#
+#function action_deploy() {
+#
+#}
+
+
 
 if [[ ${action} == "deploy" ]]; then
     echo "deploy"
 fi
 
 if [[ ${action} == "build" ]]; then
-     pwd
      echo "build"
+    # action_build
 fi
---
+++ dev/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+dir_name=$(pwd)
+
